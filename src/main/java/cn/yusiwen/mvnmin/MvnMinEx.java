00db84ab788d851437fb005f7d844101e5a20597
Siwen Yu <yusiwen@gmail.com>
2026-04-27 03:13:58 +0000

    Add --exclude option to --help output

--- src/main/java/cn/yusiwen/mvnmin/MvnMinEx.java
+++ src/main/java/cn/yusiwen/mvnmin/MvnMinEx.java
@@ -1,5 +1,6 @@
 package cn.yusiwen.mvnmin;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,13 @@ public final class MvnMinEx {
 	}
 
 	public static void main(final String[] args) {
+		for (String arg : args) {
+			if ("--help".equals(arg)) {
+				printUsage(System.out);
+				return;
+			}
+		}
+
 		List<String> excludePatterns = new ArrayList<>();
 		List<String> cleanedArgs = new ArrayList<>();
 
@@ -48,5 +56,36 @@ public final class MvnMinEx {
 
 		int exitCode = MvnMinCli.run(repo, cleanedArgs.toArray(new String[0]), System.out, true);
 		System.exit(exitCode);
+	}
+
+	private static void printUsage(final PrintStream out) {
+		out.println("usage: mvmin [options] [<maven goal(s)>] [<maven phase(s)>] [<maven arg(s)>]");
+		out.println();
+		out.println("  Project Activation/Deactivation");
+		out.println("    --all                      Activate all `pom.xml` files in all sub directories");
+		out.println("                               (default max depth: 6)");
+		out.println("    --diff[=commit[..commit]]  Activate all projects changed since the specified commit,");
+		out.println("                               or range of specified commits.");
+		out.println("                               (default: 'master')");
+		out.println("    -pl,--projects <arg>       Comma-delimited list of specified reactor projects");
+		out.println("                               to build as well as those otherwise activated.");
+		out.println("                               A project can be specified by `groupId:artifactId`");
+		out.println("                               A project can be deactivated by leading with an");
+		out.println("                               exclamation mark or hyphen: `-groupId:artifactId`");
+		out.println("    --nbi                      No build-if dependencies are considered, just");
+		out.println("                               changed modules.");
+		out.println("    --exclude <pattern>        Exclude files matching the given glob pattern(s) from");
+		out.println("                               dirty/diff detection. Comma-separated for multiple");
+		out.println("                               patterns. Prefix with ! to re-include.");
+		out.println();
+		out.println("  Scripting");
+		out.println("    -p                         Don't invoke maven, print out activated projects,");
+		out.println("                               sorted, newline separated.");
+		out.println();
+		out.println("  Debug");
+		out.println("    -d --dry-run               Don't invoke maven, print out the commands that");
+		out.println("                               would have been executed.");
+		out.println("       --version               Print the version number of mvnmin and exit.");
+		out.println();
 	}
 }
